7ce32906a8710051b6c6090a3ac99b71d5e419fa
Dmitriy Kirichenko <damamaty@gmail.com>
2019-09-02 18:15:39 +0700

    updated brew.sh with different utils
    good utils: ripgrep, bat, xsv, fd, prettyping, htop, exa
    coreutils for dircolors (required for fd)
    kitty, alacrity - gpu rendered terminals
    zsh - newer version

--- brew.sh
+++ brew.sh
@@ -1,6 +1,20 @@
 brew install ansifilter
 brew install tmux
+brew install zsh
 brew install neovim
 brew install homebrew/cask-fonts/font-firacode-nerd-font
 brew install fzf
+brew install ripgrep
 brew install jq
+brew install bat
+brew install xsv
+brew install fd
+brew install prettyping
+brew install coreutils
+brew install exa
+brew install htop
+brew cask install kitty
+brew cask install alacritty
+
+
+$(brew --prefix)/opt/fzf/install
