f8afa7a05e6493be25990b0b892ac5ac521313c4
Dmitriy Kirichenko <damamaty@gmail.com>
2019-10-20 11:55:38 +0700

    brew: added a lot of cast installs

--- brew.sh
+++ brew.sh
@@ -3,6 +3,7 @@
 brew install openssl
 brew install readline
 brew install coreutils
+brew install zlib
 
 # Utils
 
@@ -23,10 +24,31 @@ brew install lsd
 brew install htop
 brew install pgcli
 brew install reattach-to-user-namespace
+brew install ctags
 
 # Apps
 
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install iterm2
+brew cask install parallels
+brew cask install postico
+brew cask install fantastical
+brew cask install vlc
+brew cask install steam
+brew cask install virtualbox
+brew cask install postman
+brew cask install hubstaff
+brew cask install lingon-x
+brew cask install bettertouchtool
+brew cask install alfred
+brew cask install firefox
+brew cask install karabiner-elements
 brew cask install kitty
 brew cask install alacritty
+brew cask install vimr
+
 
 $(brew --prefix)/opt/fzf/install
