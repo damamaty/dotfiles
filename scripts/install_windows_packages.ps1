7554d4273ffe24bb486e50263f99902b0e4fe144
Dmitriy Kirichenko <dmitriy.kirichenko@pinogy.com>
2025-09-23 14:41:13 +0700

    added initial winget install script
    for windows machines

---
+++ scripts/install_windows_packages.ps1
@@ -0,0 +1,111 @@
+# Must Have
+winget install --id=Mozilla.Firefox -e
+winget install --id Obsidian.Obsidian -e
+winget install UpNote.UpNote
+winget install --id Bitwarden.Bitwarden -e
+winget install --id=Telegram.TelegramDesktop  -e
+winget install --id=SublimeHQ.SublimeText.4 -e
+winget install -e --id qBittorrent.qBittorrent
+winget install --id Valve.Steam -e
+winget install --id=Appest.TickTick  -e
+winget install --id=wez.wezterm  -e
+winget install MPC-BE.MPC-BE
+
+# Essential
+winget install 2dust.v2rayN
+winget install -e AntibodySoftware.WizTree
+winget install --id=M2Team.NanaZip
+winget install --id=Flow-Launcher.Flow-Launcher
+winget install --id=SyncTrayzor.SyncTrayzor  -e
+winget install --id=AutoHotkey.AutoHotkey  -e
+winget install -e --id Microsoft.PowerToys
+
+# Misc
+winget install --id=VideoLAN.VLC  -e
+winget install --id=Google.Chrome  -e
+winget install WinSCP.WinSCP
+
+# Development
+winget install --id=Microsoft.VisualStudioCode  -e
+winget install --id=TablePlus.TablePlus  -e
+winget install --id=Fork.Fork -e
+winget install --id=Anysphere.Cursor  -e
+winget install Codeium.Windsurf
+winget install --id=Perforce.P4Merge  -e
+winget install --id=ArduinoSA.IDE.stable
+winget install Postman.Postman
+winget install Atlassian.Sourcetree
+
+# Terminal tools
+winget install twpayne.chezmoi
+winget install --id=JesseDuffield.lazygit -e
+winget install --id=Neovim.Neovim -e
+winget install -e --id Git.Git
+winget install SQLite.SQLite
+winget install CoreyButler.NVMforWindows
+winget install BurntSushi.ripgrep.MSVC
+winget install --id=sharkdp.fd  -e
+winget install JFLarvoire.Ag
+winget install junegunn.fzf
+
+# System tools
+winget install Klocman.BulkCrUninstaller
+winget install Safing.Portmaster
+winget install Rufus.Rufus
+
+# Vendor Lock
+winget install Microsoft.Office
+winget install --id=Mega.MEGASync  -e
+winget install --id=Yandex.Disk -e
+winget install Adobe.Acrobat.Reader.64-bit
+winget install Blizzard.BattleNet
+
+# Hardware
+winget install -e --id CrystalDewWorld.CrystalDiskMark
+winget install CPUID.CPU-Z
+winget install REALiX.HWiNFO
+winget install -e CrystalDewWorld.CrystalDiskInfo.ShizukuEdition
+
+# Devices / Drivers
+# winget install VirtualDesktop.Streamer
+# winget install Logitech.GHUB
+# winget install Guru3D.Afterburner
+# winget install Google.PlatformTools
+# winget install Google.QuickShare
+# winget install BenjaminLSR.HandheldCompanion
+# winget install Flydigi.FlydigiSpaceStation
+# winget install Rem0o.FanControl
+# winget install WhirlwindFX.SignalRgb
+# winget install LianLi.LConnect3
+# winget install FlipperDevicesInc.qFlipper
+# winget install VirtualDesktop.Streamer
+# winget install Logitech.GHUB
+# winget install webosbrew.webOSDevManager
+# winget install FxSound.FxSound
+
+# Work
+winget install -e --id SlackTechnologies.Slack
+winget install Netsoft.Hubstaff
+
+# Other
+winget install ElementLabs.LMStudio
+
+
+# Deprecated
+# winget install --id=Alacritty.Alacritty -e
+# winget install --id WireGuard.WireGuard -e
+# winget install -e --id Microsoft.WindowsTerminal.Preview
+# winget install --id=StandardNotes.StandardNotes  -e
+# winget install -e MSYS2.MSYS2
+# winget install --id=ShareX.ShareX
+# winget install --id=Greenshot.Greenshot
+# winget install --id=Klocman.BulkCrapUninstaller
+# winget install --id=Samsung.SamsungMagician
+# winget install --id=PostgreSQL.pgAdmin -e
+# winget install --id=Atlassian.Sourcetree  -e
+#
+# winget install --id SaaSGroup.Tower -e
+# winget install --id=Notepad++.Notepad++  -e
+# winget install --id=Discord.Discord  -e
+# winget install --id=Microsoft.Skype  -e
+# winget install --id=mcmilk.7zip-zstd  -e
